b156740dfd963f73ceaa412e97ad2ee6a6d938b6
Sahil Garg <sgarg@new.local>
2014-10-06 12:48:38 +0530

    Initial Commit

---
+++ Tenderlicious/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tenderlicious
+//
+//  Created by Sahil Garg on 06/10/14.
+//  Copyright (c) 2014 cogniter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tenderlicious/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tenderlicious
+//
+//  Created by Sahil Garg on 06/10/14.
+//  Copyright (c) 2014 cogniter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
